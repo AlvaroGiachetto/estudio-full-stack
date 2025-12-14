10137c9ce296a1719491efa54fc0ceada9e037cb
Alvaro Giachetto <alvarogiachettoo@gmail.com>
2025-12-14 18:41:23 -0300

    feat: add arrays, loops and first todo-list

---
+++ javascript/07-arrays-loops/07-lesson/07-arrays-loops.js
@@ -0,0 +1,27 @@
+/*const myArray = [10, 20, 30];
+
+// First Value
+myArray[0]
+
+myArray[0] = 11;
+
+console.log(myArray);
+
+[1, 'hello', true, { name: 'socks' }, [1, 2]]
+
+Array.isArray([1, 2])
+myArray.length
+
+
+// Add at the end
+myArray.push(100);
+
+//  Remove index - value
+myArray.splice(0, 1);*/
+
+let i = 1;
+
+while (i <= 5) {
+  console.log(i);
+  i += 1; 
+}
