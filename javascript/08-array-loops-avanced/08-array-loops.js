6cce806516a39e53fb6a0a0b57da20b026972a7a
Alvaro Giachetto <alvarogiachettoo@gmail.com>
2025-12-22 15:31:48 -0300

    feat: add anonymous function

--- javascript/08-array-loops-avanced/08-array-loops.js
+++ javascript/08-array-loops-avanced/08-array-loops.js
@@ -2,7 +2,19 @@ function greeting() {
   console.log('hello');
 }
 
+
+
 // Arrow function
 const sum = (a, b) => {
   return a + b;
 };
+
+// Anonymous function
+const function1 = function() {
+  console.log('hello2');
+};
+
+console.log(function1);
+console.log(typeof function1);
+function1();
+
